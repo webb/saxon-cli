784a10c4158223975bfb3770989dde386c980f12
Webb Roberts <webb.roberts@gtri.gatech.edu>
2015-02-06 12:34:46 -0500

    autoconf integrated

--- unconfigured.mk
+++ unconfigured.mk
@@ -1,14 +1,19 @@
 
 default:
-	@ echo No default target. Targets:
-	@ echo " " zero: remove everything that can be generated
+	@ printf "No default target. Targets:\n"
+	@ printf "    zero: remove everything that can be generated\n"
+	@ printf "    init: create everything that can be generated pre-configure\n"
 
 zero:
-	$(RM) -r .gradle autom4te.cache
-	$(RM) Makefile autm4te.cache autoscan.log config.log configure share
+	$(RM) -r .gradle autom4te.cache out
+	$(RM) Makefile autm4te.cache autoscan.log config.log configure 
 	$(RM) config.guess config.sub config.status install-sh ltmain.sh
 	$(RM) depcomp missing aclocal.m4 
 	find . -type f -name '*~' -delete
 
-init:
+install-sh:
+	glibtoolize -icf
+	$(RM) ltmain.sh config.guess config.sub
+
+init: install-sh
 	autoreconf --install --verbose
